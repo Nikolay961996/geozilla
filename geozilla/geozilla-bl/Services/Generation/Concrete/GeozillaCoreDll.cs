46d43a58be95e1eb6894ee1b8fa2c5723868e190
fed-igor <fed-igor-1@ya.ru>
2024-06-13 21:38:26 +0300

    remove unnecessary platform name GeozillaCoreDll class

--- geozilla/geozilla-bl/Services/Generation/Concrete/GeozillaCoreDll.cs
+++ geozilla/geozilla-bl/Services/Generation/Concrete/GeozillaCoreDll.cs
@@ -4,19 +4,13 @@ namespace geozilla_bl.Services.Generation.Concrete
 {
     internal partial class GeozillaCoreDll
     {
-#if X86
-        private const string PlatformName = "x86";
-#else
-        private const string PlatformName = "x64";
-#endif
-
 #if DEBUG
         private const string ConfigName = "Debug";
 #else
         private const string ConfigName = "Release";
 #endif
 
-        private const string DllName = $"../../../../geozilla-core/{PlatformName}/{ConfigName}/geozilla-core.dll";
+        private const string DllName = $"../../../../geozilla-core/x64/{ConfigName}/geozilla-core.dll";
 
         [LibraryImport(DllName, EntryPoint = "?GenerateGeoJson@@YAPEBDPEBD@Z")]
         [UnmanagedCallConv(CallConvs = [typeof(System.Runtime.CompilerServices.CallConvCdecl)])]
