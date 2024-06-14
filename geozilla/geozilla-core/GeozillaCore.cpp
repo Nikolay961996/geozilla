cc3580d8c117020fb6372ba9cfbf5e4f2ce365f9
fed-igor <fed-igor-1@ya.ru>
2024-06-14 20:32:45 +0300

    import cesium native libs to geozilla-core

--- geozilla/geozilla-core/GeozillaCore.cpp
+++ geozilla/geozilla-core/GeozillaCore.cpp
@@ -1,6 +1,12 @@
 #include "GeozillaCore.h"
 
+#include <Cesium3DTilesReader/TilesetReader.h>
+
 #include <string>
+#include <vector>
+#include <cstddef>
+#include <fstream>
+#include <filesystem>
 
 namespace
 {
@@ -14,10 +20,28 @@ const char* ConvertToRawMemory(const std::string& data)
     return buffer;
 }
 
+std::vector<std::byte> ReadFile(const std::filesystem::path& fileName)
+{
+    std::ifstream file(fileName, std::ios::binary | std::ios::ate);
+    std::streamsize size = file.tellg();
+    file.seekg(0, std::ios::beg);
+
+    std::vector<std::byte> buffer(static_cast<size_t>(size));
+    file.read(reinterpret_cast<char*>(buffer.data()), size);
+
+    return buffer;
+}
+
 } // namespace
 
 const char* GenerateGeoJson(const char* path)
 {
+    auto data = ReadFile("F:/personal/Hackaton/Resources/FGM_HACKATON/tileset_hacaton.json");
+    auto* tilesetReader = new Cesium3DTilesReader::TilesetReader();
+    auto tileResult = tilesetReader->readFromJson(data);
+    if (!tileResult.value)
+        return nullptr;
+
     std::string result = "{}";
     return ConvertToRawMemory(result);
 }
